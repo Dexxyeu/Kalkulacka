b2b4a0f47a2826c1e166d836705d65031fbace13
Adam Bejček <Adam.Bejcek@student.souepl.cz>
2023-12-12 13:44:49 +0100

    kalkulacka

---
+++ kalkulacky.py
@@ -0,0 +1,22 @@
+x = input("zadejte promenou x")
+y = input("zadejte promenou y")
+
+x= int(x)
+y= int(y)
+
+print("pro sčítání zadejte +") 
+print("pro odečítání zadejte -")
+print("pro násobení zadejte *")
+print("pro dělení zadejte /")
+print("pro mocnění zadejte **")
+print("pro odmocnění zadejte /*")
+
+znamenko = ("zvolte si operatora")
+
+if znamenko == "+"
+vysledek = x + y
+vysledek =str(vysledek)
+print("vysledek je : " + vysledek)
+elif znamenko == "-"
+
+vysledek =str(vysledek)
